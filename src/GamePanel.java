2b2df93b6ad57183ecc3ef4aa49b2c2ace1cf12c
Arthur Lage <arthurlage2006@gmail.com>
2022-08-10 23:00:43 -0300

    add controls to snake, score text, collision checking

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -11,34 +11,167 @@ public class GamePanel extends JPanel implements ActionListener {
     static final int DELAY = 75;
     static int x[] = new int[GAME_UNITS];
     static int y[] = new int[GAME_UNITS];
+    int bodyParts = 6;
+    int applesEaten = 0;
+    int appleX;
+    int appleY;
+    boolean running = false;
+    Random random;
+    Timer timer;
+    char direction = 'D';
+    int score = 0;
 
     GamePanel () {
+        random = new Random();
 
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setFocusable(true);
+        this.setBackground(Color.black);
+        this.addKeyListener(new MyKeyAdapter());
+        startGame();
     }
 
-    public void startGame(){}
+    public void startGame(){
+        createApple();
+        running = true;
 
-    public void paintComponent(Graphics g) {}
+        timer = new Timer(DELAY, this);
 
-    public void draw(Graphics g) {}
+        timer.start();
+    }
 
-    public void move() {}
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
 
-    public void checkApple() {}
+    public void createApple () {
+        appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
+    }
 
-    public void checkCollision () {}
+    public void draw(Graphics g) {
+        for(int i = 0; i < SCREEN_HEIGHT; i++) {
+            g.drawLine(i*UNIT_SIZE,0, i*UNIT_SIZE, SCREEN_HEIGHT);
+            g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+        }
+
+        g.setColor(Color.red);
+        g.fillRect(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+        for (int i = 0; i < bodyParts; i++) {
+            if(i == 0) {
+                g.setColor(Color.green);
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            } else {
+                g.setColor(new Color(45, 100, 0));
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+        }
+
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g2.setColor(Color.white);
+        g2.drawString("Score: " + score, 0, 10);
+    }
+
+    public void move() {
+        for(int i = bodyParts; i > 0; i--){
+            x[i] = x[i-1];
+            y[i] = y[i-1];
+        }
+
+        switch (direction) {
+            case 'W':
+                y[0] = y[0] - UNIT_SIZE;
+                break;
+            case 'D':
+                x[0] = x[0] + UNIT_SIZE;
+                break;
+            case 'S':
+                y[0] = y[0] + UNIT_SIZE;
+                break;
+            case 'A':
+                x[0] = x[0] - UNIT_SIZE;
+                break;
+        }
+    }
+
+    public void checkApple() {
+        for(int i = bodyParts; i > 0; i--) {
+            if(x[i] == appleX && y[i] == appleY) {
+                createApple();
+                bodyParts++;
+                score++;
+            }
+        }
+    }
+
+    public void checkCollision () {
+        for(int i = bodyParts; i>0; i--) {
+            if ((x[0]) == x[i] && y[0] == y[i]) {
+                running = false;
+                break;
+            }
+        }
+
+        if (x[0] < 0) {
+            running = false;
+        }
+        if(x[0] > SCREEN_WIDTH) {
+            running = false;
+        }
+        if (y[0] < 0) {
+            running = false;
+        }
+        if(y[0] > SCREEN_HEIGHT) {
+            running = false;
+        }
+
+        if(!running) {
+            timer.stop();
+        }
+    }
 
     public void gameOver(Graphics g) {}
 
     public class MyKeyAdapter extends KeyAdapter {
         @Override
         public void keyPressed (KeyEvent e) {
-
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_A:
+                    if(direction != 'D') {
+                        direction = 'A';
+                    }
+                    break;
+                case KeyEvent.VK_D:
+                    if(direction != 'A') {
+                        direction = 'D';
+                    }
+                    break;
+                case KeyEvent.VK_S:
+                    if(direction != 'W') {
+                        direction = 'S';
+                    }
+                    break;
+                case KeyEvent.VK_W:
+                    if(direction != 'S') {
+                        direction = 'W';
+                    }
+                    break;
+            }
         }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if (running) {
+            move ();
+            checkApple();
+            checkCollision();
+        }
+        repaint();
     }
 }
